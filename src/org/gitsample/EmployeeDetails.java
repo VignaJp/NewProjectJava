8262e39ee02cb4d92a66f68a82ddf35348798f9b
VignaJp <vignagarden18@gmail.com>
2021-10-17 23:10:16 -0400

    Some Project added

---
+++ src/org/gitsample/EmployeeDetails.java
@@ -0,0 +1,15 @@
+package org.gitsample;
+
+public class EmployeeDetails 
+{
+          private void employeeName() 
+          {
+			System.out.println("Employee Name:Vigneshwari");
+
+		}
+          public static void main(String[] args) 
+          {
+			EmployeeDetails e = new EmployeeDetails();
+			e.employeeName();
+		}
+}
